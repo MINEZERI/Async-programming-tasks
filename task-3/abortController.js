f940809660deccd101f4cb8f7d2bd10b6776a8d6
Даник <pistriuga.danylo@lll.kpi.ua>
2024-12-24 03:14:42 +0200

    task 3 map use example

--- task-3/abortController.js
+++ task-3/abortController.js
@@ -26,3 +26,23 @@ function map(arr, callback, finalCallback, abortSignal) {
     });
   });
 }
+
+const controller = new AbortController();
+const signal = controller.signal;
+
+setTimeout(() => controller.abort(), 500); // Скасувати після 500 мс
+
+map(
+  [1, 2, 3],
+  (num, callback) => {
+    setTimeout(() => callback(null, num * 2), 1000);
+  },
+  (err, results) => {
+    if (err) {
+      console.error("Error:", err.message); // Операція була скасована
+    } else {
+      console.log("Results:", results);
+    }
+  },
+  signal,
+);
