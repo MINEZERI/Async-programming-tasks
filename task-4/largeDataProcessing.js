db23281c111e18ae1bdfdd905802cb8c73dea414
Даник <pistriuga.danylo@lll.kpi.ua>
2024-12-24 03:35:21 +0200

    task 4 iterator use example

--- task-4/largeDataProcessing.js
+++ task-4/largeDataProcessing.js
@@ -4,3 +4,14 @@ async function* asyncIterator(arr) {
     yield el * 2; // Обробка елемента
   }
 }
+
+async function processLargeData(arr) {
+  const iterator = asyncIterator(arr);
+
+  console.log("Processing...");
+  for await (const el of iterator) {
+    console.log("Processed:", el);
+  }
+}
+
+processLargeData([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
